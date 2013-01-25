d39b22022a0823f65e9d8626e5e517ce82851a68
moutie <mouti3chehaider@gmail.com>
2013-01-25 16:20:16 +0100

    add new gate zigbee

---
+++ GATE_ZigBee/build/CMakeFiles/GATEZigbee_pub.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "../msg_gen"
+  "../msg_gen"
+  "../src/GATE_ZigBee/msg"
+  "CMakeFiles/GATEZigbee_pub.dir/src/talker_new.o"
+  "../bin/GATEZigbee_pub.pdb"
+  "../bin/GATEZigbee_pub"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/GATEZigbee_pub.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
